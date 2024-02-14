48bdef1c7eebc35d18c3a2b9fd537dc8f2fc2e91
田中泰申 <k022g0122@g.neec.ac.jp>
2024-02-14 11:05:56 +0900

    06_01

--- main.cpp
+++ main.cpp
@@ -23,60 +23,26 @@ using namespace std;
 
 void display(int i) { cout << i << endl; }
 
-//void Qudruple(int num) {
-//	num *= 4;
-//	std::cout << "thread id = " << std::this_thread::get_id() << std::endl;
-//	std::cout << "num * 4 = " << num << std::endl;
-//
-//}
-
-//void AddFour(int num) {
-//	num += 4;
-//	std::cout << "thread id = " << std::this_thread::get_id() << std::endl;
-//	std::cout << "num + 4 = " << num << std::endl;
-//
-//}
-
 void DivisionThree(int num) {
 	num /= 3;
-	//std::cout << "thread id = " << std::this_thread::get_id() << std::endl;
 	std::cout << "thread " << num << std::endl;
 
 }
 
 void SubtractionOne(int num) {
 	num -= 1;
-	//std::cout << "thread id = " << std::this_thread::get_id() << std::endl;
 	std::cout << "thread " << num << std::endl;
 
 }
 
 void MultiplicationOne(int num) {
 	num *= 1;
-	//std::cout << "thread id = " << std::this_thread::get_id() << std::endl;
 	std::cout << "thread " << num << std::endl;
 
 }
 
 int main() {
 	int num = 3;
-
-	//std::function<void(int)>Triple = [=](int num) {
-	//	num *= 3;
-	//	printf("num * 3: %d\n", num);
-	//};
-
-		
-	//std::function<void(int)>AddThree = [=](int num) {
-	//	num += 3;
-	//	printf("num * 3: %d\n", num);
-	//};
-
-	//std::thread th1(Triple, num);
-	//std::thread th2(AddThree, num);
-
-	//std::thread th1(Double, num);
-	//std::thread th2(AddTwo, num);
 
 	std::thread th1(DivisionThree, num);
 
